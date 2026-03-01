ba97325c51ea55d0752df26551cc103e3cc6fa28
Blueberry labs <luu@blueberrylabs.com>
2026-03-01 22:30:42 +0700

    update typeorm to connect to mysql

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,8 +10,12 @@ import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 @Module({
   imports: [
     TypeOrmModule.forRoot({
-      type: 'sqlite',
-      database: 'todo.db',
+      type: 'mysql',
+      host: process.env.DB_HOST ?? '127.0.0.1',
+      port: Number(process.env.DB_PORT ?? 3306),
+      username: process.env.DB_USER ?? 'root',
+      password: process.env.DB_PASSWORD ?? '',
+      database: process.env.DB_NAME ?? 'todo_db',
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: true, // Auto-create tables (dev only)
     }),
