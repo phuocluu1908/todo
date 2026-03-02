3191587a7e1612254b58cf655de6752b9c2f814d
Blueberry labs <luu@blueberrylabs.com>
2026-03-02 23:33:27 +0700

    test: enhance TodoService tests with repository mocks

--- src/todo/todo.controller.spec.ts
+++ src/todo/todo.controller.spec.ts
@@ -9,28 +9,45 @@ import { ActivityLog } from './activity-log.entity';
 describe('TodoService', () => {
   let service: TodoService;
   let repo: Repository<Todo>;
+  let userRepo: Repository<User>;
+  let logRepo: Repository<ActivityLog>;
 
   beforeEach(async () => {
+    const todoRepoMock = {
+      create: jest.fn((obj) => ({ ...obj })),
+      save: jest.fn(),
+      findOne: jest.fn(),
+      find: jest.fn(),
+    } as any;
+    const userRepoMock = {
+      findOne: jest.fn(),
+    } as any;
+    const logRepoMock = {
+      create: jest.fn(),
+      save: jest.fn(),
+      find: jest.fn(),
+    } as any;
+
     const module: TestingModule = await Test.createTestingModule({
       providers: [
         TodoService,
-        {
-          provide: getRepositoryToken(Todo),
-          useClass: Repository,
-        },
-        {
-          provide: getRepositoryToken(User),
-          useClass: Repository,
-        },
-        {
-          provide: getRepositoryToken(ActivityLog),
-          useClass: Repository,
-        },
+        { provide: getRepositoryToken(Todo), useValue: todoRepoMock },
+        { provide: getRepositoryToken(User), useValue: userRepoMock },
+        { provide: getRepositoryToken(ActivityLog), useValue: logRepoMock },
       ],
     }).compile();
 
     service = module.get<TodoService>(TodoService);
     repo = module.get<Repository<Todo>>(getRepositoryToken(Todo));
+    userRepo = module.get<Repository<User>>(getRepositoryToken(User));
+    logRepo = module.get<Repository<ActivityLog>>(getRepositoryToken(ActivityLog));
+
+    // Provide basic mocks for Repository methods used by the service
+    if (!repo.create) (repo as any).create = jest.fn((obj) => obj);
+    if (!repo.save) (repo as any).save = jest.fn();
+    if (!userRepo.findOne) (userRepo as any).findOne = jest.fn();
+    if (!logRepo.create) (logRepo as any).create = jest.fn((obj) => obj);
+    if (!logRepo.save) (logRepo as any).save = jest.fn();
   });
 
   it('should be defined', () => {
@@ -48,7 +65,12 @@ describe('TodoService', () => {
 
       const result = await service.createTodo(createTodoDto as any, userId);
       expect(result).toEqual(savedTodo);
-      expect(repo.save).toHaveBeenCalledWith({ ...createTodoDto, userId });
+      expect(repo.save).toHaveBeenCalledWith(
+        expect.objectContaining({
+          ...createTodoDto,
+          user: { id: userId },
+        }),
+      );
     });
   });
 
--- src/todo/todo.service.spec.ts
+++ src/todo/todo.service.spec.ts
@@ -1,12 +1,26 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TodoService } from './todo.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Todo } from './todo.entity';
+import { User } from '../user/user.entity';
+import { ActivityLog } from './activity-log.entity';
 
 describe('TodoService', () => {
   let service: TodoService;
 
   beforeEach(async () => {
+    const todoRepoMock = { create: jest.fn(), save: jest.fn(), findOne: jest.fn(), find: jest.fn() } as any;
+    const userRepoMock = { findOne: jest.fn() } as any;
+    const logRepoMock = { create: jest.fn(), save: jest.fn(), find: jest.fn() } as any;
+
     const module: TestingModule = await Test.createTestingModule({
-      providers: [TodoService],
+      providers: [
+        TodoService,
+        { provide: getRepositoryToken(Todo), useValue: todoRepoMock },
+        { provide: getRepositoryToken(User), useValue: userRepoMock },
+        { provide: getRepositoryToken(ActivityLog), useValue: logRepoMock },
+      ],
     }).compile();
 
     service = module.get<TodoService>(TodoService);
